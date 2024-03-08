2758a2785176c687873b3f9fbd2280f97dfd0021
luizfilipe <luizkato8@Gmail.com>
2024-03-08 11:24:38 -0400

    criando as entitys do banco

--- src/main/kotlin/br/com/alura/alugames/dados/JogoDAO.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/JogoDAO.kt
@@ -1,10 +1,21 @@
 package br.com.alura.alugames.dao
 
-import br.com.alura.alugames.dados.Banco
+import br.com.alura.alugames.dados.JogoEntity
 import br.com.alura.alugames.model.Jogo
+import javax.persistence.EntityManager
 
-class JogoDAO {
-    fun insertJogos(jogo: Jogo) {
+class JogoDAO(manager: EntityManager): DAO<Jogo, JogoEntity>(manager, JogoEntity::class.java) {
+
+    override fun toEntity(objeto: Jogo): JogoEntity{
+        return JogoEntity(objeto.titulo, objeto.capa, objeto.preco, objeto.descricao, objeto.id)
+    }
+
+    override fun toModel(entity: JogoEntity): Jogo {
+        return Jogo(entity.titulo, entity.capa, entity.preco, entity.descricao, entity.id)
+    }
+
+
+    /*fun insertJogos(jogo: Jogo) {
         val conexao = Banco.obterConexao()
         val sql = "INSERT INTO jogos(TITULO, CAPA, PRECO, DESCRICAO) VALUES (?,?,?,?)"
         if(conexao != null){
@@ -57,5 +68,5 @@ class JogoDAO {
             }
         }
         return listaJogos
-    }
+    }*/
 }
